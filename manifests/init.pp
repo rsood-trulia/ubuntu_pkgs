d54219eee2c8da4c1c53ec33d4268355bd1d595b
Chris Mague <github@mague.com>
2015-08-17 16:20:20 -0700

    Update init.pp
    rquires apt first now

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,13 @@
 class ubuntu_pkgs {
 
+  include apt
+
   $pkgs = hiera_array('ubuntu_pkgs', [])
 
   each($ubuntu_pkgs::pkgs) |$mypkg| {
     package { $mypkg:
-      ensure => installed,
+      ensure  => installed,
+      require => Class['apt'],
     }
   }
 
